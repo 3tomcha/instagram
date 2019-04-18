2c9c5dba3748dcb6e9e83efb8840ca15bc49a997
Tomoya Kobayashi <to_kobayashi@gmail.com>
2019-04-18 12:21:25 +0900

    toukou no hozon ga dekita

--- Desktop/instagram/app/Http/Controllers/PostController.php
+++ Desktop/instagram/app/Http/Controllers/PostController.php
@@ -3,14 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Article;
 
 class PostController extends Controller
 {
     public function create(){
       return \view('create');
     }
-    public function store(){
-      echo "store";
-      // return \view('create');
+
+    public function store(Request $request){
+      $upload_file_name = $request->file('image')->store('images');
+
+      $article = new Article;
+      $article->caption = $request->input('caption');
+      $article->image = basename($upload_file_name);
+      $article->user_id = auth()->user()->id;
+      $article->save();
+      return \view('create');
     }
 }
--- Desktop/instagram/resources/views/create.blade.php
+++ Desktop/instagram/resources/views/create.blade.php
@@ -1,4 +1,5 @@
-<form class="" action="/posts" method="post"><br>
+<form class="" action="/posts" method="post" enctype="multipart/form-data"><br>
+  @csrf
 <input type="text" name="caption" value=""><br>
 <input type="file" name="image" value=""><br>
 <input type="submit">
